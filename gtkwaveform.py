6b0c6345cf07a055898eb561bc25c7c353562333
Pierre <stackp@online.fr>
2009-02-19 23:28:05 +0100

    Fancier selection graphic style.

--- gtkwaveform.py
+++ gtkwaveform.py
@@ -166,9 +166,32 @@ class SelectionLayer(object):
                                                  width, height)
             c = cairo.Context(self._cache)
             start, end = self._selection.get_selection()
-            c.set_source_rgba(1, 1, 1, 0.3)
-            c.rectangle(start, 0, end - start, height)
-            c.fill()
+
+            if start != end:
+
+                if start > end:
+                    start, end = end, start
+                    
+                # darken everything ...
+                c.set_source_rgba(0, 0, 0, 0.75)
+                c.paint()
+
+                # ... then clear selection
+                c.set_operator(cairo.OPERATOR_CLEAR)
+                c.rectangle(start + 0.5, 0, end - start + 0.5, height)
+                c.fill()
+
+
+            # cursors
+            c.set_operator(cairo.OPERATOR_ADD)
+            c.set_line_width(1)
+            c.set_source_rgba(1, 1, 1, 0.7)
+            c.move_to(start + 0.5, 0)
+            c.line_to(start + 0.5, height)
+            c.move_to(end + 0.5, 0)
+            c.line_to(end + 0.5, height)
+            c.stroke()
+            
         context.set_source_surface(self._cache, 0, 0)
         context.set_operator(cairo.OPERATOR_OVER)
         context.paint()
