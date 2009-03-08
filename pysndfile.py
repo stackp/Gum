72fb93474de56e5fd20f2c9d5e7da34b33fb4f16
Pierre <stackp@online.fr>
2009-03-08 14:31:30 +0100

    Removes license header from pysndfile

--- pysndfile.py
+++ pysndfile.py
@@ -1,8 +1,3 @@
-# Scalpel sound editor (http://stackp.online.fr/?p=48)
-# Copyright 2008 (C) Pierre Duquesne <stackp@online.fr>
-# Licensed under the Python Software Foundation License
-# (http://www.python.org/psf/license/)
-
 #! /usr/bin/env python
 # Last Change: Wed Oct 03 05:00 PM 2007 J
 
