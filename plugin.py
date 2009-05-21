f466ed44bb9d03e0b197253838dc16c270bddffc
Pierre <stackp@online.fr>
2009-05-21 14:05:18 +0200

    Remove unused files.

--- plugin.py
+++
@@ -1,5 +0,0 @@
-# Scalpel sound editor (http://stackp.online.fr/?p=48)
-# Copyright 2008 (C) Pierre Duquesne <stackp@online.fr>
-# Licensed under the Python Software Foundation License
-# (http://www.python.org/psf/license/)
-
