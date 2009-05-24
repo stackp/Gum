92b044f9660e118b03fa13f74a660c6bb1261977
Pierre <stackp@online.fr>
2009-05-24 14:16:28 +0200

    Add setup.py script.

---
+++ scalpel/__init__.py
@@ -0,0 +1,7 @@
+"""A sound editor.
+
+Scalpel is a sound editor. It works only on Linux at this point, with
+a GTK frontend and an ALSA backend.
+
+"""
+__version__ = '0.2.0'
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import scalpel
+
+doclines = scalpel.__doc__.split("\n")
+classifiers = """\
+Development Status :: 3 - Alpha
+Environment :: X11 Applications :: GTK
+Intended Audience :: End Users/Desktop
+License :: OSI Approved :: BSD License
+Natural Language :: English
+Operating System :: POSIX :: Linux
+Programming Language :: Python
+Topic :: Multimedia :: Sound/Audio :: Editors
+"""
+
+setup(name = 'scalpel',
+      version = scalpel.__version__,
+      description = doclines[0],
+      author = 'Pierre',
+      author_email = 'stackp@online.fr',
+      url = 'http://stackp.online.fr/?p=48',
+      license = "BSD License",
+      long_description = "\n".join(doclines[2:]),
+      classifiers = filter(None, classifiers.split("\n")),
+      packages = ['scalpel'],
+      scripts = ['scripts/scalpel'],
+      requires = ['PyGTK', 'numpy', 'pyalsaaudio (==0.4)'],
+      install_requires = ['pyalsaaudio==0.4']
+      )
