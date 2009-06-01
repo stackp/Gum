7b24174bde74a753c078aae746d081a2c4cada18
Pierre <stackp@online.fr>
2009-06-01 19:23:28 +0200

    Switch to BSD License.

--- scalpel/graphmodel.py
+++ scalpel/graphmodel.py
@@ -1,7 +1,6 @@
 # Scalpel sound editor (http://stackp.online.fr/?p=48)
 # Copyright 2008 (C) Pierre Duquesne <stackp@online.fr>
-# Licensed under the Python Software Foundation License
-# (http://www.python.org/psf/license/)
+# Licensed under the BSD License.
 
 from event import Signal
 
--- scalpel/run.py
+++ scalpel/run.py
@@ -2,8 +2,7 @@
 
 # Scalpel sound editor (http://stackp.online.fr/?p=48)
 # Copyright 2008 (C) Pierre Duquesne <stackp@online.fr>
-# Licensed under the Python Software Foundation License
-# (http://www.python.org/psf/license/)
+# Licensed under the BSD License.
 
 from gtkui import MainWindow
 from control import Controller
