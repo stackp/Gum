942a66231d678a01279cc10f8574869397e69e01
Pierre <stackp@online.fr>
2010-11-29 21:52:15 +0100

    Add a GTK Dialog for effects and use it in the 'Volume' effect.

--- scalpel/fx/volume.py
+++ scalpel/fx/volume.py
@@ -1,44 +1,7 @@
 import effect
-import gtk
+import gtkeffect
 
-class AbortException(Exception):
-    pass
-
-
-class VolumeDialog(gtk.Dialog):
-
-    volume = 100
-
-    def __init__(self, parent):
-        gtk.Dialog.__init__(self, title="Volume",
-                   flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
-                   buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
-                            gtk.STOCK_APPLY, gtk.RESPONSE_ACCEPT))
-        self.set_default_response(gtk.RESPONSE_ACCEPT)
-        self.set_transient_for(parent)
-
-        self.adjustment = gtk.Adjustment(self.__class__.volume, 0, 200, 1)
-        scale = gtk.HScale(self.adjustment)
-        scale.set_draw_value(False)
-        spin = gtk.SpinButton(self.adjustment, climb_rate=1, digits=0)
-        label = gtk.Label("%")
-
-        hbox = gtk.HBox()
-        hbox.set_border_width(5)
-        hbox.pack_start(scale, True, True)
-        hbox.pack_start(spin, False, False)
-        hbox.pack_end(label, False, False)
-        self.vbox.pack_start(hbox, expand=True, fill=True, padding=10)
-
-    def get_volume(self):
-        self.show_all()
-        response = self.run()
-        self.hide()
-        if response != gtk.RESPONSE_ACCEPT:
-            raise AbortException
-        self.__class__.volume = self.adjustment.get_value()
-        return self.__class__.volume
-
+volume_last = 100
 
 def volume(sound, start, end):
 
@@ -49,14 +12,19 @@ def volume(sound, start, end):
         sound.paste(start, end, y)
         
     def ui(parent):
-        dialog = VolumeDialog(parent)
+        global volume_last
+        name_param = 'Volume'
+        d = gtkeffect.Dialog('Volume')
+        d.set_transient_for(parent)
+        d.add_slider(name_param, volume_last, 0, 200)
         try:
-            volume = dialog.get_volume()
-        except AbortException:
+            parameters = d.get_parameters()
+        except:
             return
-        else:
-            process(volume)
-    
+        volume = parameters[name_param]
+        volume_last = volume
+        process(volume)
+
     return ui
 
 
---
+++ scalpel/gtkeffect.py
@@ -0,0 +1,61 @@
+import gtk
+
+class AbortException(Exception):
+    pass
+
+class Dialog(gtk.Dialog):
+    def __init__(self, title=""):
+        gtk.Dialog.__init__(self, title,
+                   flags=gtk.DIALOG_MODAL | gtk.DIALOG_DESTROY_WITH_PARENT,
+                   buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT,
+                            gtk.STOCK_APPLY, gtk.RESPONSE_ACCEPT))
+
+        self.parameters = {}
+        self.set_decorated(False)
+        self.resize(400, 1)
+        self.set_icon(self.render_icon(gtk.STOCK_CUT, gtk.ICON_SIZE_DIALOG))
+        self.table = gtk.Table(3, 2)
+        self.table.set_col_spacings(10)
+        self.table.set_row_spacings(10)
+        self.table.set_border_width(20)
+        self.vbox.pack_start(self.table, expand=False, fill=False)
+
+    def add_slider(self, name, value=5, lower=0, upper=10, ndigits=0):
+
+        adj = gtk.Adjustment(value, lower, upper)
+        self.parameters[name] = adj
+
+        vposition = len(self.parameters) - 1
+
+        label = gtk.Label(name + " :")
+        align = gtk.Alignment(1, 1, 0, 0)
+        align.add(label)
+        self.table.attach(align, 0, 1, vposition, vposition + 1,
+                          xoptions=gtk.FILL)
+
+        scale = gtk.HScale(adj)
+        scale.set_digits(ndigits)
+        scale.set_draw_value(True)
+        scale.set_restrict_to_fill_level(True)
+        self.table.attach(scale, 1, 2, vposition, vposition + 1,
+                          xoptions=gtk.EXPAND|gtk.FILL)
+
+
+    def get_parameters(self):
+        self.show_all()
+        response = self.run()
+        self.hide()
+        if response != gtk.RESPONSE_ACCEPT:
+            raise AbortException
+        values = {}
+        for name in self.parameters:
+            adj = self.parameters[name]
+            values[name] = adj.get_value()
+        return values
+
+
+if __name__ == '__main__':
+    d = Dialog('Effect')
+    d.add_slider("Delay", 500, 0, 5000)
+    d.add_slider("Feedback", 50, 0, 99)
+    print d.get_parameters()
